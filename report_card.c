747d910162ae965708dcc78ba8954b6d828e6b11
byungKHee <byungheekang@g.skku.edu>
2021-03-15 20:58:28 +0900

    add science

--- report_card.c
+++ report_card.c
@@ -4,8 +4,10 @@ int main(){
 	int korean = 95;
 	int english = 90;
 	int math = 100;
+	int science = 80;
 	printf("korean is %d\n",korean);
 	printf("english is %d\n",english);
 	printf("math is %d\n",math);
+	printf("science is %d\n",science);
 	return 0;
 }
