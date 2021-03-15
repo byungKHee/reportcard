c89ab010ec0db408755f0e2d5ac84a800ad4e745
byungKHee <byungheekang@g.skku.edu>
2021-03-15 20:42:36 +0900

    report card : Add base code

---
+++ report_card.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int korean = 95;
+	int english = 90;
+	int math = 100;
+	printf("korean is %d\n",korean);
+	printf("english is %d\n",english);
+	printf("math is %d\n",math);
+	return 0;
+}
