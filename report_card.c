c106dd31bdc3f6fa767effcdb6a5cf3545c42708
byungKHee <byungheekang@g.skku.edu>
2021-03-15 21:02:27 +0900

    add social

--- report_card.c
+++ report_card.c
@@ -4,8 +4,10 @@ int main(){
 	int korean = 95;
 	int english = 90;
 	int math = 100;
+	int social = 50;
 	printf("korean is %d\n",korean);
 	printf("english is %d\n",english);
 	printf("math is %d\n",math);
+	printf("social is %d\n",social);
 	return 0;
 }
